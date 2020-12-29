44b4d3180da98fed5e6dc063919497f2788d4991
Sascha Mann <git@mail.saschamann.eu>
2020-12-29 16:44:53 +0100

    Format the mess

--- actions/parse-push/__tests__/main.test.ts
+++ actions/parse-push/__tests__/main.test.ts
@@ -2,19 +2,19 @@
 // https://github.com/actions/setup-go/tree/3b4dc6cbed1779f759b9c638cb83696acea809d1)
 //
 // The MIT License (MIT)
-// 
+//
 // Copyright (c) 2018 GitHub, Inc. and contributors
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -44,12 +44,13 @@ describe('commit construction functions', () => {
     describe('pull_request_html_url', () => {
         it('correctly extracts the HTML URL from a commit with a single associated PR', () => {
             const commit = '1ec45d4ca4ccae5c3a68c2cf319d29c8ab4028e2'
-            nock('https://api.github.com').persist()
+            nock('https://api.github.com')
+                .persist()
                 .get(`/repos/exercism/problem-specifications/commits/${commit}/pulls`)
                 .replyWithFile(200, path.join(fixtures, 'associated-pulls-merged.json'))
-            
+
             console.log(main.getPullRequestHTMLURL(commit))
-            
+
             expect(1).toEqual(1)
 
             nock.cleanAll()
